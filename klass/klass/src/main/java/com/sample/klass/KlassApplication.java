383767ad3417d1d6dc8c4feaecc6999b8900ff6d
권영준 <dudwnss48@naver.com>
2020-09-23 16:14:07 +0900

    kwon - test

---
+++ klass/klass/src/main/java/com/sample/klass/KlassApplication.java
@@ -0,0 +1,13 @@
+package com.sample.klass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlassApplication.class, args);
+	}
+
+}
